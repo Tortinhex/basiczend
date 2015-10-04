5d73aedd5438c2508e392f38361c0c70f556ad41
Tortinhex <danilo.dorotheu@live.com>
2015-10-03 22:42:56 -0300

    Manutencao de parametros que foram modificados erroneamente no Application/Module.php

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -7,12 +7,20 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-namespace SONBase;
+namespace Application;
 
+use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 
 class Module
 {
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
